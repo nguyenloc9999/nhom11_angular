f37d7a1a1d23601d2e90ba6241aae2a1ebed2098
kientt <trankien3002721@gamil.com>
2023-05-22 21:32:44 +0700

    fix detailproduct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-import { DetailpageComponent } from './pages/detailpage/detailpage.component';
+import { DetailproductComponent } from './pages/detailproduct/detailproduct.component';
+
 
 const routes: Routes = [
   { path: 'product', component: ProductListComponent },
-  { path: 'product/:id', component: DetailpageComponent},
+  { path: 'product/:id', component: DetailproductComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-import { DetailpageComponent } from './pages/detailpage/detailpage.component';
+import { DetailproductComponent } from './pages/detailproduct/detailproduct.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DetailpageComponent } from './pages/detailpage/detailpage.component';
     HeaderComponent,
     FooterComponent,
     ProductListComponent,
-    DetailpageComponent
+    DetailproductComponent,
+
   ],
   imports: [
     BrowserModule,
