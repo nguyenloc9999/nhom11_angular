91ff7a8b81671987b0e31307e633f8da4ebf5859
tuannmph27409 <tuannmph27409@fpt.edu.vn>
2023-05-22 09:38:38 +0700

    add new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductComponent } from './pages/admin/product/product.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   // Ứng dụng phía máy chủ: (admin)
-  {
-    path: 'admin',
-    component: LayoutAdminComponent,
-    children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'products', component: ProductComponent },
-    ],
-  },
+  // {
+  //   path: 'admin',
+  //   component: LayoutAdminComponent,
+  //   children: [
+  //     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  //     { path: 'dashboard', component: DashboardComponent },
+  //     { path: 'products', component: ProductComponent },
+  //   ],
+  // },
 
-  // ứng dụng phía khách:
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      { path: 'register', component: RegisterComponent },
-    ],
-  },
+  // // ứng dụng phía khách:
+  // {
+  //   path: '',
+  //   component: LayoutComponent,
+  //   children: [
+  //     { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //     { path: 'home', component: HomeComponent },
+  //     {
+  //       path: 'login',
+  //       component: LoginComponent,
+  //     },
+  //     { path: 'register', component: RegisterComponent },
+  //   ],
+  // },
 
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent,
+  // },
+  
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
