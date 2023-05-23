c9ed28a0011950cb5f367c6dee4e56e1f1d167ad
nguyenloc <nguyenloc31102003@gmail.com>
2023-05-23 15:57:24 +0700

    productlist

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -11,11 +11,11 @@ const products: Product[] = [
     },
     {
       id: '2',
-      name: 'Giày Loafer',
+      name: 'Giày Loafer nam',
       price: 800,
       description: 'đẹp',
       imgUrl:
-        'https://luxurypro.vn/wp-content/uploads/2023/03/giay-loafer-burberry-8.jpg',
+        'https://forshoes.vn/wp-content/uploads/2022/11/IMG_4654.jpg',
     },
     {
       id: '3',
@@ -24,6 +24,30 @@ const products: Product[] = [
       description: 'đẹp',
       imgUrl:
         'https://giayda360.com/wp-content/uploads/2018/06/IMG_6796-scaled.jpg',
+    },
+    {
+      id: '4',
+      name: 'Giày SlingBack',
+      price: 350,
+      description: 'đẹp',
+      imgUrl:
+        'https://product.hstatic.net/200000068876/product/162nue__1__c31bc63355ab4359ac24f2a26bf5e3f1_master.jpg',
+    },
+    {
+      id: '5',
+      name: 'Giày Loafer nữ',
+      price: 1500000,
+      description: 'đẹp',
+      imgUrl:
+        'https://product.hstatic.net/200000033444/product/2_5bbf10f98d9144c2a79b2fc41766dcc8_grande.jpg',
+    },
+    {
+      id: '6',
+      name: 'Giày cao gót mũi nhọn',
+      price: 1000000,
+      description: 'đẹp',
+      imgUrl:
+        'https://mekongoo.com/media/catalog/product/cache/1/image/662x/9df78eab33525d08d6e5fb8d27136e95/9/4/94009XX00080_1.jpg',
     },
   ];
 
