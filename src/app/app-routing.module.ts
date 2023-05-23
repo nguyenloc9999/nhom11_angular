ff5676e46fd0937b7e121329a30fada62579c713
nguyenloc <nguyenloc31102003@gmail.com>
2023-05-23 22:08:16 +0700

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailproductComponent } from './pages/detailproduct/detailproduct.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 const routes: Routes = [
   // Ứng dụng phía máy chủ: (admin)
   // {
@@ -37,8 +39,9 @@ const routes: Routes = [
   //   path: '**',
   //   component: NotFoundComponent,
   // },
-  
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductListComponent },
+  {path: 'product/:id', component: DetailproductComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundComponent },
